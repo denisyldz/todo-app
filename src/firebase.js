50019f7728c70ceaa1aff77fbd96b2310dbdd96d
Deniz YILDIZ <100310188+denisyldz@users.noreply.github.com>
2024-07-31 13:00:47 +0300

    env created

--- src/firebase.js
+++ src/firebase.js
@@ -3,21 +3,17 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration from environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyDuKpYusxtZlzFWki1bMoFgqEI-42QlR5A",
-  authDomain: "todo-app-9cb1c.firebaseapp.com",
-  projectId: "todo-app-9cb1c",
-  storageBucket: "todo-app-9cb1c.appspot.com",
-  messagingSenderId: "30709981133",
-  appId: "1:30709981133:web:9fe9bdc7a5d0cde7686e84"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-
 export const auth = getAuth();
